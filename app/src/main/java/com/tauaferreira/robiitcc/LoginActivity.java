2336e85a71c97098bf49b95c6f7a3d42ecfacb30
tauz-hub <tauasanto101@gmail.com>
2022-09-23 22:40:10 -0300

    Initial commit

---
+++ app/src/main/java/com/tauaferreira/robiitcc/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.tauaferreira.robiitcc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/tauaferreira/robiitcc/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.tauaferreira.robiitcc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getBaseContext(), LoginActivity.class));
+            finish();
+        },5000);
+    }
+}
